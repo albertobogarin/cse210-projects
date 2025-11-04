26b912adc0e74ebfef7d310bac9d984ef2824e4b
albertobogarin <dbogarinaranda@byupathway.edu>
2025-11-04 20:03:00 -0300

    Added stubs for Journal Design activity

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        Journal myJournal = new Journal();
+        Entry testEntry = new Entry();
+        testEntry.Display();
+
+        Console.WriteLine("Program executed successfully!");
     }
 }
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>();
+
+    public string GetRandomPrompt()
+    {
+
+        return "What was the best part of your day?";
+    }
+}
